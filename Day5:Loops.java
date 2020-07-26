59509de74987404271da0aef65cfcc923c4d4890
Riddhi Suteri <ridsuteri@gmail.com>
2020-07-26 12:32:20 +0530

    Day:5

---
+++ Day5:Loops.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int N;
+        N = scan.nextInt();
+        
+        for (int i=1; i < 11; i++) {
+            int sum = N*i;
+            System.out.println(N + " x " + i + " = " + sum);
+        }
+    }
+}
