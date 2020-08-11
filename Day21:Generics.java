1fb10aea7756e3c74e704b9eb58caeb510cdf554
Riddhi Suteri <ridsuteri@gmail.com>
2020-08-11 08:00:18 +0530

    Day 21

---
+++ Day21:Generics.java
@@ -0,0 +1,9 @@
+// Write your code here
+
+    static <E>  void printArray( E[] inputArray)
+    {
+        for( E e : inputArray)
+        {
+            System.out.println(""+e);
+        }
+    }
