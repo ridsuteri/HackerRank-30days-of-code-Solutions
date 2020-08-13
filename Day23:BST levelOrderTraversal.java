b72e85dd2494fcdea31dc511015d36ce83ec39ab
Riddhi Suteri <ridsuteri@gmail.com>
2020-08-13 09:29:08 +0530

    Day 23

---
+++ Day23:BST levelOrderTraversal.java
@@ -0,0 +1,13 @@
+static void levelOrder(Node root){
+      Queue<Node> queue = new LinkedList<Node>();
+      queue.add(root);
+      while(queue.peek()!=null)
+          {
+          Node node =queue.remove();
+          System.out.print(""+node.data+" ");
+          if(node.left!=null)
+              queue.add(node.left);
+          if(node.right!=null)
+              queue.add(node.right);
+      }
+  }
